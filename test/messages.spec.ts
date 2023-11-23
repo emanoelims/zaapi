8aea659bf7fcf45a8f1af93775b683ca21c608f5
Emanoel Mendes <emanoelims@hotmail.com>
2023-11-23 00:25:04 -0300

    feat: initialize API configuration in messages.spec.ts
    Updated messages.spec.ts to include initializeConfig function. This is to facilitate proper configuration of the Z_API settings using environmental variables. Previously there was no provision for API initialization in the test suit. Now, each test setup will have API configuration using environment variables.

--- test/messages.spec.ts
+++ test/messages.spec.ts
@@ -1,8 +1,15 @@
 import * as zaapi from '../lib'
 import { expect, test } from 'vitest'
 import dotenv from 'dotenv'
+import { initializeConfig } from '../lib/config'
 
 dotenv.config()
+
+initializeConfig({
+  Z_API_INSTANCE_ID: process.env.Z_API_INSTANCE_ID || '',
+  Z_API_INSTANCE_TOKEN: process.env.Z_API_ISTANCE_TOKEN || '',
+  Z_API_CLIENT_TOKEN: process.env.Z_API_CLIENT_TOKEN || ''
+})
 
 test('should send a text message', async () => {
   const response = await zaapi.messages.sendText({
